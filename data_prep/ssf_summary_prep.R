4cf42f67fa8041f3e1d956acf49fe5521d7a6ad0
patchcervan <francerper@gmail.com>
2021-06-18 10:24:14 +0200

    Updated ssf coefficients to the 10 random point fit

--- data_prep/ssf_summary_prep.R
+++ data_prep/ssf_summary_prep.R
@@ -1,6 +1,6 @@
 
 # Read in movement kernel in RDS format imported from cape_vulture_cr project
-ssf_fit_summary <- readRDS("../vultRmap_data_aux/ssf_fit_summary.rds")
+ssf_fit_summary <- readRDS("../vultRmap_data_aux/ssf_fit_summ_10pp.rds")
 
 # Save as data
 usethis::use_data(ssf_fit_summary, overwrite = TRUE, version = 3)
