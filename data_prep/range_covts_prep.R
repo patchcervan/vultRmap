fa96b9d6af3bd5e93ea843d6caeb3d0668f667cc
patchcervan <francerper@gmail.com>
2021-06-24 16:41:52 +0200

    Updated range covariates

--- data_prep/range_covts_prep.R
+++ data_prep/range_covts_prep.R
@@ -1,24 +1,17 @@
+library(vultRmap)
 
+rm(list = ls())
 
 # Prepare covariates ------------------------------------------------------
 
 # Load covariates
 range_covts <- readRDS("../vultRmap_data_aux/df_hab_general.rds")
 
-# Clean useless columns
-# identical(range_covts$y, range_covts$lat)
-# identical(round(range_covts$y, 3), range_covts$lat)
-
-range_covts <- range_covts %>%
-  dplyr::select(!dplyr::contains("dist")) %>%
-  dplyr::select(!dplyr::contains("sl_")) %>%
-  dplyr::select(!dplyr::all_of(c("x", "y")))
-
 
 # Prepare scaling factors -------------------------------------------------
 
 # Model fit
-ssf_fit <- readRDS("../vultRmap_data_aux/ssf_fit_dist_tnoon.rds")
+ssf_fit <- readRDS("../vultRmap_data_aux/ssf_fit_10pp.rds")
 
 sds <- unlist(sapply(ssf_fit$frame, attr, "scaled:scale"))
 
