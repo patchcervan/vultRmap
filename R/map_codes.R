5a87e5a4f888bae8057a32d87cee84f57b6f8ebd
patchcervan <francerper@gmail.com>
2021-06-25 18:59:35 +0200

    Added the codes of the maps used for defining the range of the covts and sims

---
+++ R/map_codes.R
@@ -0,0 +1,7 @@
+#' @title Map codes
+#' @description These are the codes of the maps used for fitting the SSF model
+#' and that define most of the distribution range of the Cape Vulture
+#' @format A character vector
+#'
+#' @details DETAILS
+"map_codes"
