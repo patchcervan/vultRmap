ed1ef365db8b4e6cd5716bf749f0f37c2cec9189
patchcervan <francerper@gmail.com>
2021-11-29 10:23:36 +0200

    Updated colonies with Boshoff et al and last Colleywobbles

--- analysis/0_colony_updates.R
+++ analysis/0_colony_updates.R
@@ -1,11 +1,21 @@
 # This script keeps record of updates to the colony data
 
+# BEFORE USING THE FUNCTIONS IN THIS SCRIPT IS STRONGLY RECOMMENDED TO
+# MAKE A BACK UP COPY OF THE COLONY DATA.
+
+# updateColonyCount() is the workhorse function to update colony data.
+# See help("updateColonyCount") for details
+
 library(vultRmap)
+library(dplyr)
 
 rm(list = ls())
 
 col_old <- read.csv("../vultRmap_data_aux/colony_data.csv")
-
+# col_other <- read.csv("../vultRmap_data_aux/colony_data (copy).csv")
+#
+# col_old$id[!col_old$id %in% col_other$id]
+# col_other$id[!col_other$id %in% col_old$id]
 
 # Updates 15-07-2021 ------------------------------------------------------
 
@@ -15,7 +25,7 @@ colonies <- c("da_227", "da_228", "da_223", "da_225", "da_194")
 for(i in seq_along(colonies)){
 
   updateColonyCount(id_colony = colonies[i],
-                    ad_new = NA, prop_juv = 0.43,
+                    ad_new = NA, ncounts = NA, prop_juv = 0.43,
                     remove_col = FALSE, add_col = FALSE,
                     dir_colony = "../vultRmap_data_aux/colony_data.csv",
                     overwrite = TRUE)
@@ -28,13 +38,60 @@ colonies <- c("da_224", "cvcol229", "cvcol718")
 for(i in seq_along(colonies)){
 
   updateColonyCount(id_colony = colonies[i],
-                    ad_new = NA, prop_juv = 0.43,
+                    ad_new = NA, ncounts = NA, prop_juv = 0.43,
                     remove_col = TRUE, add_col = FALSE,
                     dir_colony = "../vultRmap_data_aux/colony_data.csv",
                     overwrite = TRUE)
 
 }
 
+
+# Updates 03-08-2021 ------------------------------------------------------
+
+# Add counts to Eastern Cape roosts that appear in Boshoff et al. 2009
+col_old %>%
+  slice(grep("Martha", col_old$name))
+
+col_old %>%
+  slice(grep("Agieskloof", col_old$name))
+
+# Remove duplicated roost
+updateColonyCount(id_colony = "cvcol719",
+                  ad_new = NA, prop_juv = 0.43,
+                  remove_col = TRUE, add_col = FALSE,
+                  dir_colony = "../vultRmap_data_aux/colony_data.csv",
+                  overwrite = TRUE)
+
+# Add 50 birds to Martha & Mary roost
+updateColonyCount(id_colony = "cvcol190",
+                  ad_new = 36, ncounts_new = 1, prop_juv = 0.43,
+                  remove_col = FALSE, add_col = FALSE,
+                  dir_colony = "../vultRmap_data_aux/colony_data.csv",
+                  overwrite = TRUE)
+
+# Add 120 birds to Agieskloof roost
+updateColonyCount(id_colony = "cvcol115",
+                  ad_new = 84, ncounts_new = 1, prop_juv = 0.43,
+                  remove_col = FALSE, add_col = FALSE,
+                  dir_colony = "../vultRmap_data_aux/colony_data.csv",
+                  overwrite = TRUE)
+
+
+# Updates 04-08-2021 ------------------------------------------------------
+
+# Add last counts for Colleywobbles
+col_old %>%
+  slice(grep("Colleywobbles", col_old$name))
+
+# The last count was 89 breeding pairs (Vulpro 2020?)
+# Considering previous counts the new average is
+round((388 * 5 + 89*2) / 6)
+
+updateColonyCount(id_colony = "cvcol379",
+                  ad_new = 353, ncounts_new = 6, prop_juv = 0.43,
+                  remove_col = FALSE, add_col = FALSE,
+                  dir_colony = "../vultRmap_data_aux/colony_data.csv",
+                  overwrite = TRUE)
 
 # Update sim files CAUTION! -----------------------------------------------
 
